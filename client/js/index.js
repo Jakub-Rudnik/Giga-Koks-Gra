51719b66c1000ecf2e09c3cbca4a8de17e79974c
D3vmotiv <czechpiotr2701@gmail.com>
2021-02-06 18:16:30 +0100

    😎😎🤙

--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,24 @@
-joinButton.addEventListener('click', () => gameNameWrapper.classList.remove('hidden'))
+// Global state
+const gameData = {
+    maxRangeValue: 100,
+    stage: 1,
+    header: 'Runda pierwsza 👮‍♂️',
+}
+
+// Methods
+function setBoard(gameInfo) {
+    gameHeader.innerText = gameInfo.header;
+}
+
+// Events listeners
+userNumber.addEventListener('keyup', () => {
+    const number = parseInt(userNumber.value)
+
+    if(isNaN(number)) {
+        userNumber.value = ''
+    } else if (number < 0 || number > gameData.maxRangeValue) {
+        userNumber.value = userNumber.value.slice(0,2)
+    }
+});
+
+setBoard(gameData)
