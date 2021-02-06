263fb76d19ab3754f386a05da4403372e0e0e7bc
D3vmotiv <czechpiotr2701@gmail.com>
2021-02-07 00:35:38 +0100

    Dzowni do mnie trombabomba

--- client/js/index.js
+++ client/js/index.js
@@ -1,24 +1,40 @@
 // Global state
-const gameData = {
+const gameState = {
     maxRangeValue: 100,
     stage: 1,
     header: 'Runda pierwsza 👮‍♂️',
+    isLoadingData: false,
 }
+
 
 // Methods
 function setBoard(gameInfo) {
     gameHeader.innerText = gameInfo.header;
 }
 
+
 // Events listeners
+// Client side listeners
 userNumber.addEventListener('keyup', () => {
     const number = parseInt(userNumber.value)
 
     if(isNaN(number)) {
         userNumber.value = ''
-    } else if (number < 0 || number > gameData.maxRangeValue) {
+    } else if (number < 0 || number > gameState.maxRangeValue) {
         userNumber.value = userNumber.value.slice(0,2)
     }
-});
+})
 
-setBoard(gameData)
+gameSubmit.addEventListener('click', event => {
+    event.preventDefault();
+    
+    if(gameState.isLoadingData) return
+    gameState.isLoadingData = true;
+
+    // @Todo request api call with userNumber.value
+})
+// Server side listeners
+// @ Todo wait for api call with new gameState and use it in setBoard method
+
+
+setBoard(gameState)
