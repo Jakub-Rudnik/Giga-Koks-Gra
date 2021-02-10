38fa3015bf325f088f3367b43bba900ffa7003bc
Jakub <56173355+Jape420@users.noreply.github.com>
2021-02-10 11:46:00 +0100

    added player menagment and guess state

--- server/index.js
+++ server/index.js
@@ -14,21 +14,45 @@ const io = require('socket.io')(http, {
 
 
 io.on('connection', socket => {
-    // jesli playerid1 jest puste to zajmij je
-    // jesli nie to wez na drugie
-    // jesli trzecie to wypiierdalaj nie ma miejsca
-    // console.log(socket.id); to jest pomocne
-
-    socket.on('user-choice', userNumber => {
-        console.log(userNumber);
-        // ogolny state w ktorym jest id player1 czy playerid2
-        // sprawdzamy czy nie wyslal drugi raz 
-        // zapisujemy
-    })
-    
     if (gameStates['siemanko'] === undefined)
     gameStates['siemanko'] = newGameState();
-    
+
+    var destination = '/client/gameOut.html'
+
+    if (gameStates['siemanko'].player1Id === -1 && gameStates['siemanko'].player2Id === -1) {
+        gameStates['siemanko'].player1Id = socket.id
+    } else if (gameStates['siemanko'].player2Id === -1) {
+        gameStates['siemanko'].player2Id = socket.id
+    } else {
+        socket.emit('redirect', destination)
+    }
+
+    socket.on('disconnect', () => {
+        if (gameStates['siemanko'].player1Id === socket.id ) {
+            gameStates['siemanko'].player1Id = -1
+        } else {
+            gameStates['siemanko'].player2Id = -1
+        }
+    });
+
+
+    socket.on('user-choice', userNumber => {
+        if (gameStates['siemanko'].player1Id === socket.id ) {
+            gameStates['siemanko'].player1Guess = userNumber
+        } else {
+            gameStates['siemanko'].player2Guess = userNumber
+        }
+
+        //console log player1guess and player2guess
+        if (gameStates['siemanko'].player1Guess != -1 && gameStates['siemanko'].player2Guess != -1)
+        console.log(gameStates['siemanko'].player1Id,gameStates['siemanko'].player1Guess);
+        console.log(gameStates['siemanko'].player2Id,gameStates['siemanko'].player2Guess);
+
+        // ogolny state w ktorym jest id player1 czy playerid2
+        // sprawdzamy czy nie wyslal drugi raz
+        // zapisujemy
+    })
+
     const playerState = getPlayerState(gameStates['siemanko']);
     socket.emit('new-game-state', JSON.stringify(playerState))
     // wlaczyc timer 15 sekund na wybor lcizby a potem porownac wyniki i odeslac nowa runde
@@ -37,4 +61,4 @@ io.on('connection', socket => {
 
 http.listen(4000, function() {
     console.log('Listening on port 4000')
-}) 
+})
