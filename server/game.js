014b3e5062b441f4fe39cd061f6d38c534fd6bbc
D3vmotiv <czechpiotr2701@gmail.com>
2021-02-11 13:56:05 +0100

    mozna pograc na dwoch graczy po stronie  serwera ale jeszcze nei obsluguje punktow

--- server/game.js
+++ server/game.js
@@ -5,18 +5,20 @@ function newGameState() {
                 playerGuess: -1,
                 playerId: -1,
                 playerRef: null,
+                playerPoints: -1,
             },
             {
                 playerGuess: -1,
                 playerId: -1,
                 playerRef: null,
+                playerPoints: -1,
             },
         ],
         gameInfo: {
             maxRangeValue: 100,
             stage: 1,
-            timeLeftToEndRound: 10,
-            gamestarted: false,
+            timeLeftToEndRound: 20,
+            gameStarted: false,
             startTimer: false,
         },
     }
@@ -25,25 +27,28 @@ function newGameState() {
 }
 
 function getPlayerState(gameState) {
-    const { gameInfo } = gameState;
-
-    let header;
-    switch(gameInfo.stage) {
-        case 1:
-            header = 'Runda pierwsza 👮‍♂️'
-            break
-        case 2:
-            header = 'Runda druga 🚓🚓🚔'
-            break
-        default:
-            break
-    } 
-
-    return {
-        maxRangeValue: gameInfo.maxRangeValue,
-        stage: gameInfo.stage,
-        header,
-    };
+    if(gameState !== undefined) {
+        const { gameInfo } = gameState;
+    
+        let header;
+        switch(gameInfo.stage) {
+            case 1:
+                header = 'Runda pierwsza 👮‍♂️'
+                break
+            case 2:
+                header = 'Runda druga 🚓🚓🚔'
+                break
+            default:
+                break
+        } 
+    
+        return {
+            timeLeftToEndRound: gameInfo.timeLeftToEndRound, 
+            maxRangeValue: gameInfo.maxRangeValue,
+            stage: gameInfo.stage,
+            header,
+        };
+    }
 }
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -14,8 +14,62 @@ const io = require('socket.io')(http, {
 })
 
 
-function gameLoop(gameState) {
+function gameLoop(gameState, ROOM_NAME) {
+    const { gameInfo, players } = gameState;
+    
+    function stageEnd() {
+        console.log("koniec rundy:" + players[0].playerGuess + " " + players[1].playerGuess)
 
+        if(gameInfo.stage == 1) {
+            gameInfo.stage = 2;
+            gameInfo.timeLeftToEndRound = 15;
+
+        } else if (gameInfo.stage == 2) {
+            gameInfo.stage = 3;
+            gameInfo.timeLeftToEndRound = 10;
+
+        } else if (gameInfo.stage == 3) {
+            gameInfo.stage = -1;
+            //koniec gry
+            if(players[0].playerPoints > players[1].playerPoints) {
+                console.log("wygral 1")
+                players[0].playerRef.emit('end-game', 'win')
+                players[1].playerRef.emit('end-game', 'lost')
+                
+            } else if (players[1].playerPoints > players[0].playerPoints) {
+                console.log("wygral 2")
+                players[1].playerRef.emit('end-game', 'win')
+                players[0].playerRef.emit('end-game', 'lost')
+                
+            } else {
+                console.log("remis")
+                players[0].playerRef.emit('end-game', 'remis')
+                players[1].playerRef.emit('end-game', 'remis')
+            }
+
+            clearInterval(gameInterval);
+            gameStates[ROOM_NAME] = undefined;
+        } else {
+            clearInterval(gameInterval);
+            gameStates[ROOM_NAME] = undefined;
+        }
+    }
+    
+    const gameInterval = setInterval(() => {
+        gameInfo.timeLeftToEndRound -= 3
+        console.log(`tl: ${gameState.gameInfo.timeLeftToEndRound}`);
+
+        if(players[0].playerGuess !== -1 && players[1].playerGuess !== -1) {
+            stageEnd();
+
+        } else if(gameInfo.timeLeftToEndRound < 0) {
+            stageEnd();
+        }
+
+        const playerState = getPlayerState(gameStates[ROOM_NAME]);
+        players[0].playerRef.emit('new-game-state', JSON.stringify(playerState))
+        players[1].playerRef.emit('new-game-state', JSON.stringify(playerState))
+    }, 3000);
 }
 
 
@@ -32,10 +86,11 @@ io.on('connection', socket => {
     if (player1Id === -1 && player2Id === -1) {
         gameStates[ROOM_NAME].players[0].playerRef = socket 
         gameStates[ROOM_NAME].players[0].playerId = socket.id
-
+        console.log(`nowy gracz1 o id: ${gameStates[ROOM_NAME].players[0].playerId}`);
     } else if (player2Id === -1) {
         gameStates[ROOM_NAME].players[1].playerRef = socket 
         gameStates[ROOM_NAME].players[1].playerId = socket.id
+        console.log(`nowy gracz2 o id: ${gameStates[ROOM_NAME].players[1].playerId}`);
 
     } else {
         socket.emit('redirect', destination)
@@ -44,7 +99,7 @@ io.on('connection', socket => {
 
     socket.on('disconnect', () => {
         // @ trzeba dodac ze drugi gracz wygrywa ktory sie nie rozlaczy
-        if (player1Id === socket.id ) {
+        if (gameStates[ROOM_NAME].players[0].playerId === socket.id ) {
             gameStates[ROOM_NAME].players[0].playerId = -1
         } else {
             gameStates[ROOM_NAME].players[1].playerId = -1
@@ -53,7 +108,7 @@ io.on('connection', socket => {
 
 
     socket.on('user-choice', userNumber => {
-        if (player1Id === socket.id ) {
+        if (gameStates[ROOM_NAME].players[0].playerId === socket.id ) {
             gameStates[ROOM_NAME].players[0].playerGuess = userNumber
         } else {
             gameStates[ROOM_NAME].players[1].playerGuess = userNumber
@@ -74,7 +129,13 @@ io.on('connection', socket => {
 
     const playerState = getPlayerState(gameStates[ROOM_NAME]);
     socket.emit('new-game-state', JSON.stringify(playerState))
+
     // wlaczyc timer 15 sekund na wybor lcizby a potem porownac wyniki i odeslac nowa runde
+    if(!gameStates[ROOM_NAME].gameInfo.gameStarted
+        && gameStates[ROOM_NAME].players[1].playerId !== -1) {
+        gameLoop(gameStates[ROOM_NAME], ROOM_NAME);
+        console.log('rozpoczelem gre');
+    }
 })
 
 
